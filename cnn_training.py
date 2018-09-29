1da67a7d622504334bc5ef3f6a5573932bd9aaff
PepMeister <davayte.sdohnem@yandex.ru>
2018-09-30 00:19:56 +0300

    +data

--- cnn_training.py
+++ cnn_training.py
@@ -3,6 +3,7 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras.models import model_from_json
 from keras.preprocessing.image import ImageDataGenerator
 
 
@@ -19,18 +20,12 @@ test_datagen = ImageDataGenerator(rescale = 1./255)
 training_set = train_datagen.flow_from_directory('./CNN_Data/training_set', target_size = (64, 64), batch_size = 32, class_mode = 'binary')
 test_set = test_datagen.flow_from_directory('./CNN_Data/test_set', target_size = (64, 64), batch_size = 32, class_mode = 'binary')
 
-cnn.fit_generator(training_set, steps_per_epoch = 8000, epochs = 25, validation_data = test_set, validation_steps = 2000)
+cnn.fit_generator(training_set, steps_per_epoch = 8000, epochs = 3, validation_data = test_set, validation_steps = 2000)
 
-import numpy as np
-from keras.preprocessing import image
-
-test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64,64))
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-
-result = cnn.predict(test_image)
-if result[0][0] == 1:
-	prediction = 'dog'
-else:
-	prediction == 'cat'
-print(prediction)
+model_json = cnn.to_json()
+print(model_json)
+with open("cnn_cat_or_dog.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+cnn.save_weights("model.h5")
+print("Saved model to disk..")
--- cnn_using.py
+++ cnn_using.py
@@ -9,19 +9,18 @@ print("Loaded model from disk..")
 
 # evaluate loaded model on test data
 cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-score = cnn.evaluate(X, Y, verbose=0)
-print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
 
 import numpy as np
 from keras.preprocessing import image
 
-test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64,64))
+test_image = image.load_img('./CNN_Data/test_set/cats/cat.4999.jpg', target_size = (64,64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 
 result = cnn.predict(test_image)
+#print(result)
 if result[0][0] == 1:
 	prediction = 'dog'
 else:
-	prediction == 'cat'
+	prediction = 'cat'
 print(prediction)
